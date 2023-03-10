5470187afb90928fc5627b1df3420a8eb8295bde
Axel Noel Cabezas Cerda <114128602+AxelCab17@users.noreply.github.com>
2023-03-09 22:24:56 -0600

    Cambio

--- ProyectoG4/src/javaapplication1/JavaApplication1.java
+++ ProyectoG4/src/javaapplication1/JavaApplication1.java
@@ -6,7 +6,7 @@ public class JavaApplication1 {
 
  
     public static void main(String[] args) {
-        System.out.println("Hola equipo");
+        System.out.println("Hola Mundo");
     }
     
 }
